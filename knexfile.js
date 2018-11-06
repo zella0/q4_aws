39184cafcdaa1b0d8c74509713e2fbf899f9f75a
Nhan Phan <xeith1712@hotmail.com>
2018-11-06 11:01:54 -0700

    for albert

--- knexfile.js
+++ knexfile.js
@@ -2,10 +2,7 @@ module.exports = {
   development: {
       client: 'pg',
       connection: {
-        database: "g96q4",
-        host: "testinstance.cx60kjmtucna.us-east-2.rds.amazonaws.com",
-        user: "xella0",
-        password: "g96q4qweasdzxc"
+  
       },
       migrations: {
           directory: __dirname + '/db/migrations',
